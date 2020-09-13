466b07b251c2b44b1cb9e24f192cd656bbec6b8d
Esenda Obey <eobey@mi9retail.com>
2020-09-14 01:25:03 +0200

    feat: removed files

--- src/type-graphql/resolvers/crud/User/args/CreateUserArgs.ts
+++
@@ -1,9 +0,0 @@
-import { Args, ArgsType, Context, Field, Float, ID, Info, InputType, Int, Mutation, ObjectType, Query, ResolveField, Resolver, Root, registerEnumType } from "@nestjs/graphql";
-import GraphQLJSON from "graphql-type-json";
-import { UserCreateInput } from "../../../inputs/UserCreateInput";
-
-@ArgsType()
-export class CreateUserArgs {
-  @Field(_type => UserCreateInput, { nullable: false })
-  data!: UserCreateInput;
-}
--- src/type-graphql/resolvers/crud/User/args/DeleteManyUserArgs.ts
+++
@@ -1,9 +0,0 @@
-import { Args, ArgsType, Context, Field, Float, ID, Info, InputType, Int, Mutation, ObjectType, Query, ResolveField, Resolver, Root, registerEnumType } from "@nestjs/graphql";
-import GraphQLJSON from "graphql-type-json";
-import { UserWhereInput } from "../../../inputs/UserWhereInput";
-
-@ArgsType()
-export class DeleteManyUserArgs {
-  @Field(_type => UserWhereInput, { nullable: true })
-  where?: UserWhereInput | undefined;
-}
